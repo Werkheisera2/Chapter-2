7b0bb6a391cc273fba9245376e345b26753f2fe0
Werkheisera2 <werkheisera2@gmail.com>
2014-09-08 16:51:26 -0400

    Completed #6 on Pg. 88

---
+++ Room.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Room
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Length = 25;
+            int Width = 10;
+            Console.WriteLine("Length:             25 feet");
+            Console.WriteLine("Width:              10 feet");
+            Console.WriteLine("___________________________");
+            Console.WriteLine("The floor space is " + Length * Width);
+            Console.WriteLine("square feet.");
+        }
+    }
+}
