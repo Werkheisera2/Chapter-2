7565bfa23a5b3ec9f1c0e1033fbf7e52441952c9
Werkheisera2 <werkheisera2@gmail.com>
2014-09-09 08:01:35 -0400

    Done with #4

--- Room.cs
+++ Room.cs
@@ -11,10 +11,11 @@ namespace Room
         {
             int Length = 25;
             int Width = 10;
+            int Area = Length * Width;
             Console.WriteLine("Length:             25 feet");
             Console.WriteLine("Width:              10 feet");
             Console.WriteLine("___________________________");
-            Console.WriteLine("The floor space is " + Length * Width);
+            Console.WriteLine("The floor space is " + Area);
             Console.WriteLine("square feet.");
         }
     }
