b255b254ed2843184cdf68e90b3788794826c460
Werkheisera2 <werkheisera2@gmail.com>
2014-09-10 16:50:14 -0400

    Finished with HoursandMinutes

---
+++ HoursandMinutes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HoursandMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Minutes = 185;
+            Console.WriteLine("Worked 185 minutes. 185 minutes=" + Minutes / 60 + " hours and " + Minutes % 60+" minutes.");
+        }
+    }
+}
