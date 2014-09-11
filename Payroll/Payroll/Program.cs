daeb46718c80b1c1d540d269f128fbdc239e7ce1
Werkheisera2 <werkheisera2@gmail.com>
2014-09-11 08:44:32 -0400

    Payroll

---
+++ Payroll/Payroll/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int total;
+            Console.Write("pls enter ur name pls k thx: ");
+            String name = Console.ReadLine();
+
+            Console.Write("pls entr ur social secrty numbr pls thx: ");
+            String Social = Console.ReadLine();
+
+            string first = Console.ReadLine();
+
+            Console.Write("hey wut is ur hourly pay r8: ");
+            input = Console.ReadLine();
+            total = Convert.ToInt32(input);
+
+            Console.Write("hey wt is ur hours worked pls tell me thx: ");
+            input = Console.ReadLine();
+            total = Convert.ToInt32(input);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            
+
+
+
+        }
+    }
+}
